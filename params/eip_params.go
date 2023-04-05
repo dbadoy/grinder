d944619772da7f75b11f5f360b85b86a9cf19b43
dbadoy4874 <dbadoy4874@gmail.com>
2023-04-05 23:28:54 +0900

    params: add params about proxy pattern EIPs

---
+++ params/eip_params.go
@@ -0,0 +1,10 @@
+package params
+
+const (
+	// https://eips.ethereum.org/EIPS/eip-1967
+	AdminAddressSlotEIP1967          = "0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103"
+	ImplementationAddressSlotEIP1967 = "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc"
+
+	// https://eips.ethereum.org/EIPS/eip-1822
+	LogicAddressSlotEIP1822 = "0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7"
+)

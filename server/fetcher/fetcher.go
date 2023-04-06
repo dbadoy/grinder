5569746680cb6dbbc2ccc9df262271e9a8738006
dbadoy4874 <dbadoy4874@gmail.com>
2023-04-06 12:52:29 +0900

    add subscribe with test codes

--- server/fetcher/fetcher.go
+++ server/fetcher/fetcher.go
@@ -1,6 +1,8 @@
 package fetcher
 
 import (
+	"context"
+	"fmt"
 	"time"
 
 	"github.com/dbadoy/grinder/pkg/checkpoint"
@@ -35,8 +37,114 @@ func New(client ethclient.Client, cp checkpoint.CheckpointReader, cfg *Config) *
 }
 
 func (f *Fetcher) Run() {
+	go f.subscribe()
 }
 
 func (f *Fetcher) Stop() {
 	f.quit <- struct{}{}
+}
+
+// subscribe subscribes to events for new blocks. If the target
+// node does not support subscription, perform the polling method.
+func (f *Fetcher) subscribe() {
+	ch := make(chan *types.Block)
+
+	// In the subscription method, recovery does not occur until
+	// the next block creation event comes in.
+	sub, err := f.eth.SubscribeNewBlock(context.Background(), ch)
+	if err == nil {
+		for {
+			select {
+			case block := <-ch:
+				f.handle(block.NumberU64())
+
+			case <-f.quit:
+				sub.Unsubscribe()
+				return
+			}
+		}
+	}
+
+	close(ch)
+	f.polling(f.cfg.PollInterval)
+}
+
+// polling checks the block number at the given interval time.
+func (f *Fetcher) polling(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			latest, err := f.eth.GetLatestBlockNumber(context.Background())
+			if err != nil {
+				continue
+			}
+
+			f.handle(latest)
+
+		case <-f.quit:
+			return
+		}
+	}
+}
+
+// handle compares the checkpoint to the blockchain's latest
+// block number and performs a stateful action.
+//
+// 1. BN == Checkpoint
+//		- Synchronized
+//
+// 2. BN == Checkpoint + 1
+//		- Synchronized, new block detected
+//
+// 3. BN > Checkpoint
+// 		- Asynchronous, data recovery between Checkpoint and BN
+//
+// 4. BN < Checkpoint
+//		- Critical error ( need to consider reconfiguration)
+func (f *Fetcher) handle(latest uint64) {
+	if latest == f.cp.Checkpoint() {
+		return
+	}
+
+	if latest == f.cp.Checkpoint()+1 {
+		block, err := f.eth.BlockByNumber(context.Background(), latest)
+		if err != nil {
+			return
+		}
+		f.C <- block
+		return
+	}
+
+	f.recover(latest)
+}
+
+// recover fetches and sends the blocks between the checkpoint
+// and the given latest block number in order. If the process
+// of fetching a particular block number fails, it will retry.
+func (f *Fetcher) recover(latest uint64) {
+	if latest > f.cp.Checkpoint() {
+		for latest != f.cp.Checkpoint() {
+			// synchronization process is a blocking operation,
+			// so we check each time if the user has ended the
+			// polling.
+			select {
+			case <-f.quit:
+				return
+			default:
+			}
+
+			block, err := f.eth.BlockByNumber(context.Background(), f.cp.Checkpoint()+1)
+			if err != nil {
+				// Retry on failure until success.
+				continue
+			}
+
+			f.C <- block
+		}
+	} else {
+		panic(fmt.Errorf("occur critical error, blockchain latest: %d checkpoint: %d", latest, f.cp.Checkpoint()))
+	}
 }

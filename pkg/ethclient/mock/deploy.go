cf2c2a24880e9cadb5d60a0af2c159d0cdb0fbdb
Seungbae Yu <dbadoy4874@gmail.com>
2023-04-06 19:56:59 +0900

    mod, pkg/ethclient: Implement a mock ethereum client (#2)
    * mod: update
    
    * pkg/ethclient: add mock client
    
    * Update mock.go
    
    * store address
    
    * changes order
    
    * export Mock, add template private(public) key
    
    * add comments
    
    * rename Core to Backend
    
    * using error that defined geth
    
    * add comments

---
+++ pkg/ethclient/mock/deploy.go
@@ -0,0 +1,56 @@
+package mock
+
+import (
+	"context"
+	"errors"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+)
+
+// DeployContract is a template method for how to use
+// backends.SimulatedBackend.
+//
+// It is describing the overall steps of 'sending a transaction
+// - creating a block - getting a receipt' process well.
+func DeployContract(m *Mock, bytecode []byte) (common.Address, error) {
+	nonce, err := m.c.NonceAt(context.Background(), m.addr, nil)
+	if err != nil {
+		return common.Address{}, err
+	}
+
+	gasPrice, err := m.c.SuggestGasPrice(context.Background())
+	if err != nil {
+		return common.Address{}, err
+	}
+
+	tx := types.NewTx(&types.LegacyTx{
+		Nonce:    nonce,
+		To:       nil,
+		Gas:      m.c.Blockchain().GasLimit(),
+		GasPrice: gasPrice,
+		Data:     bytecode,
+	})
+
+	signed, err := types.SignTx(tx, types.LatestSignerForChainID(chainID), m.priv)
+	if err != nil {
+		return common.Address{}, err
+	}
+
+	if err := m.c.SendTransaction(context.Background(), signed); err != nil {
+		return common.Address{}, err
+	}
+
+	m.c.Commit()
+
+	receipt, err := m.c.TransactionReceipt(context.Background(), signed.Hash())
+	if err != nil {
+		return common.Address{}, err
+	}
+
+	if receipt.Status == 0 {
+		return common.Address{}, errors.New("transaction failed")
+	}
+
+	return receipt.ContractAddress, nil
+}

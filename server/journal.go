1aa4e7e9dfd68ce420528efd6dd36c28c8093395
dbadoy4874 <dbadoy4874@gmail.com>
2023-04-07 10:57:17 +0900

    add some comments about journalObject

--- server/journal.go
+++ server/journal.go
@@ -2,6 +2,12 @@ package server
 
 import "github.com/dbadoy/grinder/server/cft"
 
+// journalObject has contrasting methods for specific
+// behaviors. Stored data related to blockchain rarely
+// undergoes modifications, only additions. Therefore,
+// journalObject simply implements revert, and does
+// not define behaviors such as 'rollback(to previous
+// value)'.
 type journalObject interface {
 	revert(engine cft.Engine) error
 }

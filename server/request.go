9cd1002a1f596a6f0b11e962ba679827176f8ba1
dbadoy4874 <dbadoy4874@gmail.com>
2023-04-09 18:30:28 +0900

    define `Request`

---
+++ server/request.go
@@ -0,0 +1,33 @@
+package server
+
+import (
+	"github.com/dbadoy/grinder/server/dto"
+	"github.com/ethereum/go-ethereum/common"
+)
+
+const (
+	abiRequestType = 0x01 + iota
+	contractRequestType
+)
+
+type Request interface {
+	Errorc() chan<- error
+	Kind() byte
+}
+
+type ABIRequest struct {
+	Name string
+	ABI  *dto.ABI
+	errc chan error
+}
+
+type ContractRequest struct {
+	Address common.Address
+	errc    chan error
+}
+
+func (a *ABIRequest) Errorc() chan<- error { return a.errc }
+func (ABIRequest) Kind() byte              { return abiRequestType }
+
+func (c *ContractRequest) Errorc() chan<- error { return c.errc }
+func (ContractRequest) Kind() byte              { return contractRequestType }

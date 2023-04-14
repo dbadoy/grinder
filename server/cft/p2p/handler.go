a989ff0d559ab720e74fd63852ad7ee65f73c2b1
dbadoy4874 <dbadoy4874@gmail.com>
2023-04-14 20:43:56 +0900

    add `handler`

--- server/cft/p2p/handler.go
+++ server/cft/p2p/handler.go
@@ -1,22 +1,68 @@
 package p2p
 
-func (s *Server) handleCheckpoint(peer *peer, cp *checkpointMsg) {
-	if s.cp.Checkpoint() >= cp.Checkpoint {
-		//
+import (
+	"bytes"
+
+	"github.com/dbadoy/grinder/pkg/checkpoint"
+)
+
+type handler struct {
+	cp  checkpoint.CheckpointHandler
+	cfg *Config
+}
+
+func (h *handler) validateMsg(m msg) bool {
+	if m.checkpoint() != h.cp.Checkpoint() {
+		return false
+	}
+
+	if bytes.Equal(m.clusterCode(), m.clusterCode()) {
+		return false
+	}
+
+	return true
+}
+
+func (h *handler) handleMsg(peer *peer, msg msg) {
+	if !h.validateMsg(msg) {
 		return
 	}
 
-	s.cp.SetCheckpoint(cp.Checkpoint)
+	switch msg.kind() {
+	case handshakeMsgType:
+		h.handleHandshake(peer, msg.(*handshakeMsg))
 
+	case ackMsgType:
+
+	case pingMsgType:
+		h.handlePing(peer, msg.(*pingMsg))
+
+	case pongMsgType:
+
+	case checkpointMsgType:
+		h.handleCheckpoint(peer, msg.(*checkpointMsg))
+	}
+}
+
+func (h *handler) handleHandshake(peer *peer, hs *handshakeMsg) {
 	peer.conn.writeMsg(&ackMsg{
-		ClusterCode: s.ClusterHash(),
-		Checkpoint:  s.cp.Checkpoint(),
+		ClusterCode: ClusterHash(h.cfg.Cluster),
+		Checkpoint:  h.cp.Checkpoint(),
 	})
 }
 
-func (s *Server) handleHandshake(peer *peer, hs *handshakeMsg) {
+func (h *handler) handlePing(peer *peer, p *pingMsg) {
+	peer.conn.writeMsg(&pongMsg{
+		ClusterCode: ClusterHash(h.cfg.Cluster),
+		Checkpoint:  h.cp.Checkpoint(),
+	})
+}
+
+func (h *handler) handleCheckpoint(peer *peer, cp *checkpointMsg) {
+	h.cp.SetCheckpoint(cp.Checkpoint)
+
 	peer.conn.writeMsg(&ackMsg{
-		ClusterCode: s.ClusterHash(),
-		Checkpoint:  s.cp.Checkpoint(),
+		ClusterCode: ClusterHash(h.cfg.Cluster),
+		Checkpoint:  h.cp.Checkpoint(),
 	})
 }

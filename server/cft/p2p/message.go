fa77ed06fb80107cbd6ab8071cdc200ac9f9e995
dbadoy4874 <dbadoy4874@gmail.com>
2023-04-14 20:44:07 +0900

    define `msg`

--- server/cft/p2p/message.go
+++ server/cft/p2p/message.go
@@ -2,32 +2,40 @@ package p2p
 
 const (
 	handshakeMsgType = byte(0x01) + iota
+	ackMsgType
 	pingMsgType
 	pongMsgType
-	syncMsgType
 	checkpointMsgType
 )
 
 type msg interface {
-	// kind() byte
+	kind() byte
+	clusterCode() []byte
+	checkpoint() uint64
 }
 
 type (
 	handshakeMsg struct {
+		Version     int
 		ClusterCode []byte
 		Checkpoint  uint64
 	}
 
 	ackMsg struct {
+		Version     int
 		ClusterCode []byte
 		Checkpoint  uint64
 	}
 
-	pingMsg struct{}
+	pingMsg struct {
+		ClusterCode []byte
+		Checkpoint  uint64
+	}
 
-	pongMsg struct{}
-
-	syncMsg struct{}
+	pongMsg struct {
+		ClusterCode []byte
+		Checkpoint  uint64
+	}
 
 	checkpointMsg struct {
 		ClusterCode []byte
@@ -38,3 +46,23 @@ type (
 func encodeMsg(m msg) ([]byte, error)
 
 func decodeMsg(b []byte) (msg, error)
+
+func (handshakeMsg) kind() byte             { return handshakeMsgType }
+func (h *handshakeMsg) clusterCode() []byte { return h.ClusterCode }
+func (h *handshakeMsg) checkpoint() uint64  { return h.Checkpoint }
+
+func (ackMsg) kind() byte             { return ackMsgType }
+func (a *ackMsg) clusterCode() []byte { return a.ClusterCode }
+func (a *ackMsg) checkpoint() uint64  { return a.Checkpoint }
+
+func (pingMsg) kind() byte             { return pingMsgType }
+func (p *pingMsg) clusterCode() []byte { return p.ClusterCode }
+func (p *pingMsg) checkpoint() uint64  { return p.Checkpoint }
+
+func (pongMsg) kind() byte             { return pongMsgType }
+func (p *pongMsg) clusterCode() []byte { return p.ClusterCode }
+func (p *pongMsg) checkpoint() uint64  { return p.Checkpoint }
+
+func (checkpointMsg) kind() byte             { return checkpointMsgType }
+func (c *checkpointMsg) clusterCode() []byte { return c.ClusterCode }
+func (c *checkpointMsg) checkpoint() uint64  { return c.Checkpoint }

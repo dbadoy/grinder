0ab9418dfdc67a3e23c097847310a3650e3b004c
Seungbae Yu <dbadoy4874@gmail.com>
2023-06-10 00:34:17 +0900

    server/dto: add field `RelateAddress` to Contract DTO

--- server/dto/contract.go
+++ server/dto/contract.go
@@ -3,6 +3,10 @@ package dto
 type Contract struct {
 	TxHash     string
 	Candidates []string
+
+	// We need to determine which logic contract
+	// the Proxy contract is connected to.
+	RelateAddress []string
 }
 
 func (Contract) Index() string {
